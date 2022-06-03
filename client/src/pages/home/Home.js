3fa55300dcbfab18a2a9a875fa5840b93dca9408
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-06-03 13:45:43 -0500

    Component widgetSm creado

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -2,13 +2,19 @@ import FeaturedInfo from "../../components/featureInfo/FeaturedInfo";
 import Chart from "../../components/chart/Chart";
 import "./home.css";
 import {userData} from "../../dummyData";
+import WidgetSm from "../../components/widgetSm/WidgetSm";
+import WidgetLg from "../../components/widgetLg/WidgetLg"
 
  const Home = () => {
  	return (
  		<div className="home">
  			<FeaturedInfo/>
             <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
- 		</div>
+ 		    <div className="homeWidgets">
+                <WidgetSm/>
+                <WidgetLg/>
+            </div>
+        </div>
  	)
  }
 
