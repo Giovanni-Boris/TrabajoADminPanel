b51579df80df3eae318e5fa45057f330780994a8
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-06-03 14:06:07 -0500

    user page creado

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Sidebar from "./components/sidebar/Sidebar";
 import "./app.css";
 import Home from "./pages/home/Home";
 import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
 import { BrowserRouter as Router,Routes ,Route } from 'react-router-dom'
 
 function App() {
@@ -10,11 +11,12 @@ function App() {
     <Router>
        <Topbar/>
        <div className="container">
-         <Sidebar/>
-         <Routes>
-           <Route path='/' element={<Home/>}/>
-           <Route path="/users" element={<UserList/>}/>
-         </Routes>
+        <Sidebar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path="/users" element={<UserList/>}/>
+          <Route path="/user/:userId" element={<User/>}/>
+        </Routes>
        </div>
     </Router>
   );
