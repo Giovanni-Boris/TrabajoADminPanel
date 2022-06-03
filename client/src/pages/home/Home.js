1ccd9debc3b37fee1bae441eb605e0b7b6dd0590
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-06-03 13:26:56 -0500

    Component chart o graficos creado

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,10 +1,13 @@
 import FeaturedInfo from "../../components/featureInfo/FeaturedInfo";
- import "./home.css";
+import Chart from "../../components/chart/Chart";
+import "./home.css";
+import {userData} from "../../dummyData";
 
  const Home = () => {
  	return (
  		<div className="home">
  			<FeaturedInfo/>
+            <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
  		</div>
  	)
  }
