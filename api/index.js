cd8b12eadb7b460da508064d9497634907fec293
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-06-04 18:46:55 -0500

    Creando server express y conexion a base de datos a traves de mongoos

---
+++ api/index.js
@@ -0,0 +1,16 @@
+const express= require("express");
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+const option = { useNewUrlParser: true, useUnifiedTopology: true}
+
+mongoose.connect(process.env.MONGO_URL,option)
+    .then(()=> console.log('Base de datos conectada'))
+    .catch(error => console.log('Error db: ', error))
+
+app.use(express.json())
+
+app.listen(process.env.PORT || 5000, () =>{
+    console.log("Backend server is running");
+ }); 
