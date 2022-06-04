91b4b3623f152eed1ed8de9092df67377657fc7f
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-06-04 18:54:19 -0500

    Modelo de usuario

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express= require("express");
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+dotenv.config();
 
 const option = { useNewUrlParser: true, useUnifiedTopology: true}
 
