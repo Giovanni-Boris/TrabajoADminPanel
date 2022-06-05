36561e64fa9eac1081178c866d2dd24b1794511a
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-06-04 19:35:00 -0500

    Crear usuarios por administrador

--- api/routes/verifyToken.js
+++ api/routes/verifyToken.js
@@ -1,28 +1,29 @@
 const jwt = require("jsonwebtoken")
 
 
- const verifyToken = (req,res,next)=>{
- 	const authHeader = req.header.token;
- 	if(authHeader){
- 		jwt.verify(token, process.env.JWT_SEC,(err,data)=>{
- 			if(err) return res.status(401).json("Token is not valited!");
- 			console.log(user);
- 			req.user = user; 
- 			next();
- 		})
- 	}else{
- 		return res.status(401).json("You are not authenticated!");
- 	}
- }
- //to performance and not repet this code
- const verifyTokenAndAuthorization = (req,res,next)=>{
- 	verifyToken(req,res,()=>{
- 		if(req.user.id === req.params.id || req.user.isAdmin){
- 			next();
- 		} else{
- 			res.status(401).json("You are not allowed to do that!")
- 		}
- 	})
- }
+const verifyToken = (req,res,next)=>{
+	const authHeader = req.headers.token;
+	if(authHeader){
+		const token = authHeader.split(' ')[2];
+        jwt.verify(token, process.env.JWT_SEC,(err,user)=>{
+			if(err) return res.status(401).json("Token is not valited!");
+			console.log(user);
+			req.user = user; 
+			next();
+		})
+	}else{
+		return res.status(401).json("You are not authenticated!");
+	}
+}
+//to performance and not repet this code
+const verifyTokenAndAuthorization = (req,res,next)=>{
+	verifyToken(req,res,()=>{
+		if(req.user.id === req.params.id || req.user.isAdmin){
+			next();
+		} else{
+			res.status(401).json("You are not allowed to do that!")
+		}
+	})
+}
 
  module.exports = {verifyToken, verifyTokenAndAuthorization };
