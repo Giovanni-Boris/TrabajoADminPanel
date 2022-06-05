d69df02dc3b98e0aef883eaafed993b2b9846570
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-06-04 19:14:55 -0500

    login user con crypto js para contraseñas encriptadas

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ const express= require("express");
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const authRoute = require("./routes/auth");
 dotenv.config();
 
 const option = { useNewUrlParser: true, useUnifiedTopology: true}
@@ -11,6 +12,7 @@ mongoose.connect(process.env.MONGO_URL,option)
     .catch(error => console.log('Error db: ', error))
 
 app.use(express.json())
+app.use("/api/auth", authRoute);
 
 app.listen(process.env.PORT || 5000, () =>{
     console.log("Backend server is running");
