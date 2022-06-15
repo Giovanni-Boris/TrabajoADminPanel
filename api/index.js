af670a50a0389086ac86abf94f754d0c6d5c8586
Giovanni-Boris <bancasi@unsa.edu.pe>
2022-06-14 19:22:29 -0500

    Agregando cors

--- api/index.js
+++ api/index.js
@@ -5,7 +5,7 @@ const dotenv = require('dotenv');
 const authRoute = require("./routes/auth");
 const userRoute = require('./routes/user');
 const productRoute = require("./routes/product");
-
+const cors = require("cors");
 dotenv.config();
 
 const option = { useNewUrlParser: true, useUnifiedTopology: true}
@@ -13,7 +13,12 @@ const option = { useNewUrlParser: true, useUnifiedTopology: true}
 mongoose.connect(process.env.MONGO_URL,option)
     .then(()=> console.log('Base de datos conectada'))
     .catch(error => console.log('Error db: ', error))
-
+// cors 
+const  corsOption = {
+    origin: '#',
+    optionsSuccessStatus:200
+}
+app.use(cors({corsOption}))
 app.use(express.json())
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
